02fa293ebd6d1cbfdba1ec7d99c7dcf7b2aee153
Pan Otlet <otlet0@gmail.com>
2015-02-03 00:26:19 +0100

    :disappointed_relieved:

--- visualizer.js
+++ visualizer.js
@@ -43,10 +43,12 @@ analyser.connect(audioCtx.destination);
 function play_pause(){
   if(runing){
     runing=false;
-    myAudio.pause()
+    myAudio.pause();
+	myAudio.src='';
   } else{
     runing=true;
-    myAudio.play()
+    myAudio.play();
+	myAudio.src='http://s1.thekrzos.eu:8700/;stream.mp3';	
   }
   loop();
 }
